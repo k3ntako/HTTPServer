0a2df7cafa8d7102c5f834b37f385e3c26418573
Kentaro Kaneki <kentarokaneki@gmail.com>
2020-07-02 14:49:13 -0400

    Create FileIO class that can write to file

---
+++ src/main/java/com/k3ntako/HTTPServer/FileIO.java
@@ -0,0 +1,11 @@
+package com.k3ntako.HTTPServer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileIO {
+  public void write(Path path, String str) throws IOException {
+    Files.write(path, str.getBytes());
+  }
+}
---
+++ src/test/java/com/k3ntako/HTTPServer/FileIOTest.java
@@ -0,0 +1,32 @@
+package com.k3ntako.HTTPServer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileIOTest {
+  final private Path path = Paths.get("./file.txt");
+
+  @Test
+  void write() throws IOException {
+    final var str = "This is text\nthis is a new line!";
+
+    final var fileIO = new FileIO();
+    fileIO.write(path, str);
+
+    final var fileContent = Files.readString(path);
+
+    assertEquals(str, fileContent);
+  }
+
+  @AfterEach
+  void tearDown() throws IOException {
+    Files.deleteIfExists(path);
+  }
+}
