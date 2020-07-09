85d892e87f4c00b89b40b7aa10a372c8ea091664
Kentaro <kentarokaneki@gmail.com>
2020-07-09 10:51:44 -0400

    Remove try-catch

--- src/main/java/com/k3ntako/HTTPServer/Main.java
+++ src/main/java/com/k3ntako/HTTPServer/Main.java
@@ -2,24 +2,22 @@ package com.k3ntako.HTTPServer;
 
 import com.k3ntako.HTTPServer.wrappers.ServerSocketWrapper;
 
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
-    try {
-      var serverIO = new ServerServerIO();
-      var requestHandler = new RequestHandler();
-      var serverSocket = new ServerSocketWrapper(5000);
+  public static void main(String[] args) throws IOException {
+    var serverIO = new ServerServerIO();
+    var requestHandler = new RequestHandler();
+    var serverSocket = new ServerSocketWrapper(5000);
 
-      var routeRegistrar = new RouteRegistrar(new RouteRegistry());
-      var routeRegistry = routeRegistrar.registerRoutes();
-      var router = new Router(routeRegistry);
+    var routeRegistrar = new RouteRegistrar(new RouteRegistry());
+    var routeRegistry = routeRegistrar.registerRoutes();
+    var router = new Router(routeRegistry);
 
-      var app = new Server(serverIO, requestHandler, serverSocket, router);
+    var app = new Server(serverIO, requestHandler, serverSocket, router);
 
-      while (true) {
-        app.run();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+    while (true) {
+      app.run();
     }
   }
 }
